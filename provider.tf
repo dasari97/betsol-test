45addb169f3904454814ef47cbef6825a1dc1e9b
Dasari Krishna <Dasari.Krishna@betsol.com>
2022-03-14 15:39:23 +0530

    updated tf file

--- provider.tf
+++ provider.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-terraform {
+/*terraform {
     backend "s3" {
         bucket = ""
         key = "Test/test_state_file_backup"
         region = "us-east-1"
     }
-}
+}*/
