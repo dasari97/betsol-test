1abcab21467ab292dde280cccc725e63c031d670
Dasari Krishna <Dasari.Krishna@betsol.com>
2022-03-14 15:36:16 +0530

    adding new tf files

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
-provider "aws" {
-    region = "us-east-1"  
-}
+
 
 resource "aws_vpc" "vpc" {
   cidr_block       = "10.0.0.0/24"
---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+terraform {
+    backend "s3" {
+        bucket = ""
+        key = "Test/test_state_file_backup"
+        region = "us-east-1"
+    }
+}
